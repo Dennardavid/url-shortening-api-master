dc45db61ea1163575c3f65acff2fb57a68d0453a
Dennar David <dennardavid@gmail.com>
2024-03-19 16:53:14 +0100

    Error

--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
 const apiURL = "https://cleanuri.com/api/v1/shorten";
+const submitButton = document.querySelector(".submit");
+const formData = new FormData(document.querySelector(".input-form"));
+
+submitButton.addEventListener("click", async function (event) {
+  event.preventDefault();
+  const request = await fetch(apiURL, {
+    method: "POST",
+    mode: "no-cors",
+    body: formData,
+  });
+  const response = await request.json();
+  console.log(...response);
+});
