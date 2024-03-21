7629a015e99949615c60e4ebe7a74f2296cf0c55
Dennar David <dennardavid@gmail.com>
2024-03-21 16:46:33 +0100

    finally solved this

--- src/script.js
+++ src/script.js
@@ -1,18 +1,33 @@
-const apiURL = "https://cleanuri.com/api/v1/shorten";
+// My Api key
+const tinyUrlApi =
+  "FnrfUTcvOn1k5iXxUVBho9qqX75nQvA5jwyjfnbYQbZlmx8tqIBPOtlle6cP";
 
+// API url
+const apiURL = `https://api.tinyurl.com/create/`;
+
+// Grabbing html Elements
 let formElement = document.querySelector(".input-form");
-const urlToShorten = document.querySelector("input").value;
+const urlToShorten = document.querySelector("input");
 
+console.log(urlToShorten);
+
+/* Event listener on the form to wait for submit */
 formElement.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  fetch(apiURL, {
+  /* Fetching the shortened url */
+  fetch("https://api.tinyurl.com/create/", {
     method: "POST",
     headers: {
-      Accept: "application / json",
+      accept: "application/json",
+      Authorization: `Bearer ${tinyUrlApi}`,
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ url: urlToShorten }),
+    body: JSON.stringify({
+      url: urlToShorten.value,
+      domain: "tinyurl.com",
+      description: "string",
+    }),
   })
     .then((response) => {
       if (!response.ok) {
@@ -21,6 +36,7 @@ formElement.addEventListener("submit", function (event) {
       console.log(response);
       return response.json();
     })
+    .then((data) => console.log(data))
     .catch((error) => {
       console.error("Error", error);
     });
