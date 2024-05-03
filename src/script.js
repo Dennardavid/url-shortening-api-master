534998be623216fcb7f3d00eda3a43363af46f27
Dennar David <dennardavid@gmail.com>
2024-05-03 09:09:40 +0100

    Fixed some bugs

--- src/script.js
+++ src/script.js
@@ -11,21 +11,8 @@ const urlToShorten = document.querySelector(".input");
 const linksDiv = document.querySelector(".links_shortened");
 const wrongUrl = document.querySelector("#wrong_url");
 
-/* Create elements to be displayed on the screen */
-const newDiv = document.createElement("div");
-const innerDiv = document.createElement("div");
-const shortLink = document.createElement("p");
-const originalLink = document.createElement("p");
-const copyButton = document.createElement("button");
-
 /* Regex for form validation */
 const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
-
-/* Event listener on the form to wait for submit */
-formElement.addEventListener("submit", function (event) {
-  /* URL validation */
-  validateUrlAndFetch(event);
-});
 
 /* function to validate and fetch Url from the form */
 function validateUrlAndFetch(event) {
@@ -76,32 +63,56 @@ function validateUrlAndFetch(event) {
     .catch((error) => {
       console.error("Error", error);
     });
+
+  /* Create elements to be displayed on the screen */
+  const newDiv = document.createElement("div");
+  const innerDiv = document.createElement("div");
+  const shortLink = document.createElement("p");
+  const originalLink = document.createElement("p");
+  const copyButton = document.createElement("button");
+
+  /* Function to display the shortened URLS  */
+  function disiplayShortenedUrl(data) {
+    /* Style the newly created elements */
+    shortLink.style.color = "hsl(180, 66%, 49%)";
+    newDiv.style.cssText =
+      "display:flex; justify-content:space-between; align-items:center; width:74%; background-color:white; border-radius:10px; padding:10px; padding-left:30px; padding-right:30px; margin-bottom:10px;";
+
+    originalLink.textContent = `${data.data.url}`;
+    shortLink.textContent = `${data.data.tiny_url}`;
+    innerDiv.style.cssText =
+      "display:flex; width:350px; justify-content:space-between; align-items:center;";
+    copyButton.textContent = "Copy";
+    copyButton.style.cssText =
+      "background-color:hsl(180, 66%, 49%); border-radius:5px; padding:5px; padding-left:20px; padding-right:20px; color:white; border:none;";
+
+    innerDiv.appendChild(shortLink);
+    innerDiv.appendChild(copyButton);
+
+    newDiv.appendChild(originalLink);
+    newDiv.appendChild(innerDiv);
+    linksDiv.appendChild(newDiv);
+  }
+
+  copyButton.addEventListener("click", function () {
+    copyButton.textContent = "Copied!";
+    copyButton.style.backgroundColor = "hsl(257, 27%, 26%)";
+  });
 }
 
-/* Function to display the shortened URLS  */
-function disiplayShortenedUrl(data) {
-  /* Style the newly created elements */
-  shortLink.style.color = "hsl(180, 66%, 49%)";
-  newDiv.style.cssText =
-    "display:flex; justify-content:space-between; align-items:center; width:74%; background-color:white; border-radius:10px; padding:10px; padding-left:30px; padding-right:30px; ";
-
-  originalLink.textContent = `${data.data.url}`;
-  shortLink.textContent = `${data.data.tiny_url}`;
-  innerDiv.style.cssText =
-    "display:flex; width:350px; justify-content:space-between; align-items:center;";
-  copyButton.textContent = "Copy";
-  copyButton.style.cssText =
-    "background-color:hsl(180, 66%, 49%); border-radius:5px; padding:5px; padding-left:20px; padding-right:20px; color:white; border:none;";
-
-  innerDiv.appendChild(shortLink);
-  innerDiv.appendChild(copyButton);
-
-  newDiv.appendChild(originalLink);
-  newDiv.appendChild(innerDiv);
-  linksDiv.appendChild(newDiv);
-}
-
-copyButton.addEventListener("click", function () {
-  copyButton.textContent = "Copied!";
-  copyButton.style.backgroundColor = "hsl(257, 27%, 26%)";
+/* Event listener on the form to wait for submit */
+formElement.addEventListener("submit", function (event) {
+  /* URL validation */
+  validateUrlAndFetch(event);
 });
+
+var firstName = "David";
+
+const Setemi = {
+  firstName: "Setemi",
+  greeting: () => {
+    console.log(`Hello ${this.firstName}`);
+  },
+};
+
+Setemi.greeting();
